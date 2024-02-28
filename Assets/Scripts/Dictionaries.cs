a7b4e00b039dfc23dab4ef33808089a42959af4c
KS <ks@mail.com>
2024-02-28 09:21:39 +0200

    continue dictionary-examples...

--- Assets/Scripts/Dictionaries.cs
+++ Assets/Scripts/Dictionaries.cs
@@ -12,11 +12,18 @@ public class Dictionaries : MonoBehaviour
         characterDict.Add("Edwin", 10);
         characterDict.Add("Minsc", 18);
 
+        //Changing value of key value -pair
+        characterDict["Edwin"] = 11;
+
+        characterDict["Max"] = 12; // NOTE: no error thrown if key doesn't exist!
+
+        // Adding key only if it doesn't already exist
+        characterDict.TryAdd("Edwin", 15); //won't be added, because Edwin already exists
         
     }
 
     private void Start()
     {
-        print(characterDict.Count); // 2
+        print(characterDict.Count); // 3
     }
 }
